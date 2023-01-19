a67056a3e27c423194ed1f83c1a74eb4d3f27063
optimus789 <vinaysudrik@gmail.com>
2023-01-19 22:42:10 +0530

    Added simplification

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,26 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-
+let mainHtml;
+let mainText;
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+ 
 
-  @Get('/:text')
-  getHello(@Param() text: any): string {
-    console.log(text);
-    return this.appService.getHello();
+  @Get('html')
+  getHtml(): string {
+    return mainHtml;
+  }
+  @Get('text')
+  getText(): string {
+    return mainText;
   }
 
   @Post('postData')
   postData(@Body() text: any): string {
     console.log(text);
     let plainText = text.text;
+    mainHtml = plainText;
     plainText = plainText.replace(/<style([\s\S]*?)<\/style>/gi, '');
     plainText = plainText.replace(/<script([\s\S]*?)<\/script>/gi, '');
     plainText = plainText.replace(/<\/div>/gi, '\n');
@@ -24,6 +30,7 @@ export class AppController {
     plainText = plainText.replace(/<\/p>/gi, '\n');
     plainText = plainText.replace(/<br\s*[\/]?>/gi, '\n');
     plainText = plainText.replace(/<[^>]+>/gi, '');
+    mainText = plainText;
     return this.appService.postData(plainText);
   }
 }
