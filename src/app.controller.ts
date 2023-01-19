cdc27e4413a6d39aaf365034620ff741f3509ec4
optimus789 <vinaysudrik@gmail.com>
2023-01-19 22:14:59 +0530

    cors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,23 +7,24 @@ export class AppController {
 
   @Get('/:text')
   getHello(@Param() text: any): string {
-    console.log(text)
+    console.log(text);
     return this.appService.getHello();
   }
 
   @Post('postData')
   postData(@Body() text: any): string {
     console.log(text);
+    console.log(JSON.parse(text));
     let plainText = text.text;
-    plainText =plainText.replace(/<style([\s\S]*?)<\/style>/gi, '');
+    plainText = plainText.replace(/<style([\s\S]*?)<\/style>/gi, '');
     plainText = plainText.replace(/<script([\s\S]*?)<\/script>/gi, '');
-    plainText = plainText.replace(/<\/div>/ig, '\n');
-    plainText = plainText.replace(/<\/li>/ig, '\n');
-    plainText = plainText.replace(/<li>/ig, '  *  ');
-    plainText = plainText.replace(/<\/ul>/ig, '\n');
-    plainText = plainText.replace(/<\/p>/ig, '\n');
-    plainText = plainText.replace(/<br\s*[\/]?>/gi, "\n");
-    plainText = plainText.replace(/<[^>]+>/ig, '');
+    plainText = plainText.replace(/<\/div>/gi, '\n');
+    plainText = plainText.replace(/<\/li>/gi, '\n');
+    plainText = plainText.replace(/<li>/gi, '  *  ');
+    plainText = plainText.replace(/<\/ul>/gi, '\n');
+    plainText = plainText.replace(/<\/p>/gi, '\n');
+    plainText = plainText.replace(/<br\s*[\/]?>/gi, '\n');
+    plainText = plainText.replace(/<[^>]+>/gi, '');
     return this.appService.postData(plainText);
   }
 }
